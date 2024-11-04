59b4ac3ee71a6be8255eadee53e0a904408e75d2
Pedro Igor <pedro.igor.pitl@gmail.com>
2024-11-03 23:15:15 -0300

    Correção e ADD de logo em pokemon_detail_activity.xml

--- app/src/main/java/com/example/projeto_pokedex_app/DatabaseHelper.java
+++ app/src/main/java/com/example/projeto_pokedex_app/DatabaseHelper.java
@@ -64,16 +64,4 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         return cursor;
     }
-
-    public Cursor getPokemonByGenerationAndType(int generation, String type) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_POKEMON, null,
-                COLUMN_GENERATION + "=? AND (" + COLUMN_TYPE1 + "=? OR " + COLUMN_TYPE2 + "=?)",
-                new String[]{String.valueOf(generation), type, type}, null, null, null);
-
-        // Log para depuração
-        Log.d("DatabaseHelper", "Querying for generation: " + generation + " and type: " + type + " Result Count: " + cursor.getCount());
-
-        return cursor;
-    }
 }
